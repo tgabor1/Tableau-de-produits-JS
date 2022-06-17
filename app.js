cff575b423ba368ac5a570375e49e8300b5a1655
tgabor1 <t.gabor@tutanota.com>
2022-06-17 12:33:34 +0200

    modifs

---
+++ app.js
@@ -0,0 +1,19 @@
+let tab = [prod1, prod2, prod3, prod4, prod5]
+let michael = new Product('MICHAËL GREGORIO, 43,00 €, Non, Non')
+let daniel = new Product('DANIEL GUICHARD, 43,00 €, Non, 20%')
+let msi = new Product('PC Portable Gaming MSI GL75 Leopard 10SFK- 457FR 17,3" Intel Core i7 16 Go RAM 256 Go SSD + 1 To SATA Noir, 1999,99 €, Oui, 20%')
+let asus = new Product('PC Portable Gaming Asus TUF505DV- HN232T 15.6" AMD Ryzen 7 16 Go RAM 512 Go SSD Noir, 1499,99 €, Oui, 33%')
+let acer = new Product('PC Portable Gaming Acer Predator Triton 700 PT715-51- 76D4 15.6" Gaming Intel Core i7 32 Go RAM 256 Go SSD + 256 Go SATA Noir, 3499,99 €, Non, Non')
+let spectacles = new Category('Spectacles')
+
+
+
+return this.promotion
+
+
+
+for (let i=0; i <= 4; i++) {
+
+    if (tab[i].promotion=='oui')
+    console.log(tab[i].promotion)
+}
---
+++ class.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(name, price, promotion, discount) {
+        this.name = name
+        this.price = price
+        this.promotion = promotion
+        this.discount = discount
+    }
+}
+
+class Category {
+    constructor(name) {
+        this.name = name
+    }
+}
