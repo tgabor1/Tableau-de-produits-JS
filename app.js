46ea10aff68696062db46ed685bcbae343688d4e
tgabor1 <t.gabor@tutanota.com>
2022-06-28 14:17:11 +0200

    modifs diverses

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let product2 = new Product('DANIEL GUICHARD', 'Spectacles Rodez', '43,00 €', '
 let product3 = new Product('PC Portable Gaming MSI GL75 Leopard 10SFK- 457FR 17,3" Intel Core i7 16 Go RAM 256 Go SSD + 1 To SATA Noir', 'Ordinateurs portables', '1999,99 €', 'Oui', '20%')
 let product4 = new Product('PC Portable Gaming Asus TUF505DV- HN232T 15.6" AMD Ryzen 7 16 Go RAM 512 Go SSD Noir', 'Ordinateurs portables', '1499,99 €', 'Oui', '33%')
 let product5 = new Product('PC Portable Gaming Acer Predator Triton 700 PT715-51- 76D4 15.6" Gaming Intel Core i7 32 Go RAM 256 Go SSD + 256 Go SATA Noir', 'Ordinateurs portables', '3499,99 €', 'Non', 'Non')
+// Création du tableau de produits
+let tableProducts = [product1, product2, product3, product4, product5]
 // Déclaration des catégories
 let category1 = new Category('Spectacles Rodez')
 let category2 = new Category('Ordinateurs portables')
@@ -13,8 +15,6 @@ let checkboxPromotion = document.querySelector('#checkboxPromotion')
 let checkboxDiscount = document.querySelector('#checkboxDiscount')
 let checkboxName = document.querySelector('#checkboxName')
 let checkboxCategory = document.querySelector('#checkboxCategory')
-// Création du tableau de produits
-let tableProducts = [product1, product2, product3, product4, product5]
 // Association du tableau à l'élément du DOM, ce qui correspond à ce qu'il y aura d'affiché dans le tableau
 let elementProducts = document.querySelector('#products')
 // Je crée une variable qui contiendra le tableau d'origine tel que présenté sur l'énoncé
